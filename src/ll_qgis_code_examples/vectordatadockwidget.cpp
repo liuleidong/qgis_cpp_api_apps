77bbdb6e40124bbf506813415692b14eb0977fa4
richie12 <richie12@126.com>
2023-10-10 14:12:48 +0800

    添加vectordatadockwidget类

---
+++ src/ll_qgis_code_examples/vectordatadockwidget.cpp
@@ -0,0 +1,19 @@
+﻿#include "vectordatadockwidget.h"
+#include "ui_vectordatadockwidget.h"
+
+VectorDataDockWidget::VectorDataDockWidget(QWidget *parent) :
+    QDockWidget(parent),
+    ui(new Ui::VectorDataDockWidget)
+{
+    ui->setupUi(this);
+}
+
+VectorDataDockWidget::~VectorDataDockWidget()
+{
+    delete ui;
+}
+
+QPlainTextEdit *VectorDataDockWidget::plainTextEdit()
+{
+    return ui->plainTextEdit;
+}
---
+++ src/ll_qgis_code_examples/vectordatadockwidget.h
@@ -0,0 +1,24 @@
+﻿#ifndef VECTORDATADOCKWIDGET_H
+#define VECTORDATADOCKWIDGET_H
+
+#include <QDockWidget>
+#include <QPlainTextEdit>
+
+namespace Ui {
+class VectorDataDockWidget;
+}
+
+class VectorDataDockWidget : public QDockWidget
+{
+    Q_OBJECT
+
+public:
+    explicit VectorDataDockWidget(QWidget *parent = nullptr);
+    ~VectorDataDockWidget();
+    QPlainTextEdit* plainTextEdit();
+
+private:
+    Ui::VectorDataDockWidget *ui;
+};
+
+#endif // VECTORDATADOCKWIDGET_H
