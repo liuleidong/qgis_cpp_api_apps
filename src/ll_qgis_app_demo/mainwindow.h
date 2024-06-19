79d06c7d7e827e5a926a15e47e7bd0015a4d404d
liuleidong <liuleidong@163.com>
2024-06-19 16:30:33 +0800

    add qgis_app test demo

---
+++ src/ll_qgis_app_demo/mainwindow.h
@@ -0,0 +1,26 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+#include "ll_qgis_base_lib.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    void initialize();
+
+private:
+    Ui::MainWindow *ui;
+
+    ll_qgis_base_lib *mApp = nullptr;
+};
+#endif // MAINWINDOW_H
